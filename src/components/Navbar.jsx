802d2ab01f434ecde485e26c9c001815d51ee927
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-13 17:56:23 +0500

    Add hamburger and cross icon in case screen becomes shorter, attach on click event

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import "./NavbarStyles.css"
 import { MenuItems } from './ManuItems'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 function Navbar() {
+    const [clicked, setClicked] = useState(false)
+    const toggleIcon = () => setClicked(!clicked)
   return (
     <nav className='navbarItems'>
         <h1 className='navbar-logo'>
             Trippy
         </h1>
+        <div className='menu-icons' onClick={toggleIcon}>
+            <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
         <ul className='nav-menu'>
             {MenuItems.map((item, index) => (
                 <li key={index}>
