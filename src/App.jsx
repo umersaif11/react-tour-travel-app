ac7e6d4de259e651df1eefb1034fb81506af6601
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-16 15:26:31 +0500

    Currently using the react router syntax v6 acc to tutorial video but will refactor after.... Here using Routes and within it different routes... and in main.jsx use BroswerRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/Home'
+import About from './routes/About'
+import Contact from './routes/Contact'
+import Service from './routes/Service'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/service' element={<Service/>}/>
+      </Routes>
       <Navbar/>
     </div>
   )
