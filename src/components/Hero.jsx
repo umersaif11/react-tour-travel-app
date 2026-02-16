a655d917e331711e01cbf0beb237069f76580b2a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-16 16:19:11 +0500

    Create files for hero component and styles

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <>
+        <div className='hero'>
+            <img src="" alt="HeroImage" />
+        </div>
+    </>
+  )
+}
+
+export default Hero
