714453e1e2bebdc4dd4e626cefb91252ef8520a6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-16 17:02:36 +0500

    Add image heading and paragraph in the hero componen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import './HeroStyles.css'
+import NatureImage from '../assets/12.jpg'
 function Hero() {
   return (
     <>
         <div className='hero'>
-            <img src="" alt="HeroImage" />
+            <img src={NatureImage} alt="HeroImage" />
+            <div className='hero-text'>
+                <h1>
+                    Your Journey Your Story
+                </h1>
+                <p>
+                    Choose Your Favourite Destination
+                </p>
+                <a href="/">Travel Plan</a>
+            </div>
         </div>
     </>
   )
