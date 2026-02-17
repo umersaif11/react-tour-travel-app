967fac656d056c5252a3c439f9465f635f891d65
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-17 18:46:36 +0500

    Use props in the destinationItem component

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -4,6 +4,7 @@ import Mountain1 from "../assets/1.jpg"
 import Mountain2 from "../assets/2.jpg"
 import Mountain3 from "../assets/3.jpg"
 import Mountain4 from "../assets/4.jpg"
+import DestinationItem from './DestinationItem'
 
 
 function Destination() {
@@ -13,6 +14,19 @@ function Destination() {
         <p>
             Tours give you the opportunity to see a lot, within a time frame.
         </p>
+        <DestinationItem
+        heading="Taal Volcano, Batangas"
+        text="One of the most iconic views in Luzon, Mt. 
+        Taal boasts a volcano inside a lake inside an island. 
+        If you fancy a closer look, the hike up to the crater is a mere 45 minutes, 
+        and is easy enough for beginners. Guides will assist you most of the way, 
+        and you'll see the peculiar environment found on an active volcano, 
+        including volcanic rocks and steam vents. The hike can be dusty and hot, 
+        so plan for an early morning trip, 
+        and then unwind with some bulalo before heading back home!"
+        img1={Mountain1}
+        img2={Mountain2}
+        />
     </div>
   )
 }
--- src/components/DestinationItem.jsx
+++ src/components/DestinationItem.jsx
@@ -5,29 +5,25 @@ import Mountain3 from "../assets/3.jpg"
 import Mountain4 from "../assets/4.jpg"
 import React from 'react'
 
-function DestinationItem() {
+function DestinationItem({
+    heading,
+    text,
+    img1,
+    img2
+}) {
   return (
     <div className='first-des'>
         <div className='des-text'>
-            <h2>Taal Volcano, Batangas</h2>
-            <p>
-                One of the most iconic views in Luzon, Mt. 
-                Taal boasts a volcano inside a lake inside an island. 
-                If you fancy a closer look, the hike up to the crater is a mere 45 minutes, 
-                and is easy enough for beginners. Guides will assist you most of the way, 
-                and you'll see the peculiar environment found on an active volcano, 
-                including volcanic rocks and steam vents. The hike can be dusty and hot, 
-                so plan for an early morning trip, 
-                and then unwind with some bulalo before heading back home!
-            </p>
+            <h2>{heading}</h2>
+            <p>{text}</p>
         </div>
         <div className='image'>
             <img 
-            src={Mountain1} 
+            src={img1} 
             alt="Mountain Image One" 
             />
             <img 
-            src={Mountain2} 
+            src={img2} 
             alt="Mountain Image Two" 
             />
         </div>
