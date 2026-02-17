061a8884ac5fabba95b8e6929431bf174408d909
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-17 18:19:00 +0500

    Create destination dynamic component so in destination component only one time heading and paragraph remains there while other part from destination component goes to gynamic resuable destinationItem component

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -13,31 +13,6 @@ function Destination() {
         <p>
             Tours give you the opportunity to see a lot, within a time frame.
         </p>
-        <div className='first-des'>
-            <div className='des-text'>
-                <h2>Taal Volcano, Batangas</h2>
-                <p>
-                    One of the most iconic views in Luzon, Mt. 
-                    Taal boasts a volcano inside a lake inside an island. 
-                    If you fancy a closer look, the hike up to the crater is a mere 45 minutes, 
-                    and is easy enough for beginners. Guides will assist you most of the way, 
-                    and you'll see the peculiar environment found on an active volcano, 
-                    including volcanic rocks and steam vents. The hike can be dusty and hot, 
-                    so plan for an early morning trip, 
-                    and then unwind with some bulalo before heading back home!
-                </p>
-            </div>
-            <div className='image'>
-                <img 
-                src={Mountain1} 
-                alt="Mountain Image One" 
-                />
-                <img 
-                src={Mountain2} 
-                alt="Mountain Image Two" 
-                />
-            </div>
-        </div>
     </div>
   )
 }
---
+++ src/components/DestinationItem.jsx
@@ -0,0 +1,38 @@
+import "./DestinationStyles.css"
+import Mountain1 from "../assets/1.jpg"
+import Mountain2 from "../assets/2.jpg"
+import Mountain3 from "../assets/3.jpg"
+import Mountain4 from "../assets/4.jpg"
+import React from 'react'
+
+function DestinationItem() {
+  return (
+    <div className='first-des'>
+        <div className='des-text'>
+            <h2>Taal Volcano, Batangas</h2>
+            <p>
+                One of the most iconic views in Luzon, Mt. 
+                Taal boasts a volcano inside a lake inside an island. 
+                If you fancy a closer look, the hike up to the crater is a mere 45 minutes, 
+                and is easy enough for beginners. Guides will assist you most of the way, 
+                and you'll see the peculiar environment found on an active volcano, 
+                including volcanic rocks and steam vents. The hike can be dusty and hot, 
+                so plan for an early morning trip, 
+                and then unwind with some bulalo before heading back home!
+            </p>
+        </div>
+        <div className='image'>
+            <img 
+            src={Mountain1} 
+            alt="Mountain Image One" 
+            />
+            <img 
+            src={Mountain2} 
+            alt="Mountain Image Two" 
+            />
+        </div>
+    </div>
+  )
+}
+
+export default DestinationItem
