9c95600d047525d7a69a782e688ca48f680a85e6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-18 14:49:22 +0500

    Create footer component with footr stylesheet

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FooterStyles.css'
+
+function Footer() {
+  return(
+    <div className='footer'>
+        <div className='top'></div>
+
+        <div className='bottom'></div>
+    </div>
+  )
+}
+export default Footer
