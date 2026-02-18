670e8a6f246df7462e960ce01dddcdf29a37ebaf
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-18 20:40:37 +0500

    Create form within component and put component in the Contact component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -10,6 +10,16 @@ function ContactForm() {
             type="text" 
             placeholder='Name'
             />
+            <input 
+            type="email" 
+            placeholder='Email'
+            />
+            <input 
+            type="text" 
+            placeholder='Subject'
+            />
+            <textarea placeholder='Message' rows='4'></textarea>
+            <button>Send Message</button>
         </form>
     </div>
   )
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import ContactImage from '../assets/2.jpg'
 import Footer from '../components/Footer'
+import ContactForm from '../components/ContactForm'
 function Contact() {
   return (
     <>
@@ -14,6 +15,7 @@ function Contact() {
       url='/'
       btnClass='hide'
       />
+      <ContactForm/>
       <Footer/>
     </>
   )
