7f1059195e1a0d717a9742047c378cd0a07ea5f0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-18 15:55:46 +0500

    Html structure for the footer is complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,55 @@ import './FooterStyles.css'
 function Footer() {
   return(
     <div className='footer'>
-        <div className='top'></div>
+        <div className='top'>
+            <div>
+                <h1>Trippy</h1>
+                <p>Choose your favourite destination</p>
+            </div>
+            <div>
+                <a href='/'>
+                    <i class="fa-brands fa-square-facebook"></i>
+                </a>
+                <a href='/'>
+                    <i class="fa-brands fa-square-instagram"></i>
+                </a>
+                <a href='/'>
+                    <i class="fa-brands fa-square-behance"></i>
+                </a>
+                <a href='/'>
+                    <i class="fa-brands fa-square-twitter"></i>
+                </a>
+            </div>
+        </div>
 
-        <div className='bottom'></div>
+        <div className='bottom'>
+            <div>
+                <h4>Project</h4>
+                <a href="/">Changelog</a>
+                <a href="/">Status</a>
+                <a href="/">License</a>
+                <a href="/">All Versions</a>
+            </div>
+            <div>
+                <h4>Community</h4>
+                <a href="/">Github</a>
+                <a href="/">Issues</a>
+                <a href="/">Project</a>
+                <a href="/">Twitter</a>
+            </div>
+            <div>
+                <h4>Help</h4>
+                <a href="/">Support</a>
+                <a href="/">Troubleshooting</a>
+                <a href="/">Contact Us</a>
+            </div>
+            <div>
+                <h4>Others</h4>
+                <a href="/">Terms of Service</a>
+                <a href="/">Privacy Policy</a>
+                <a href="/">License</a>
+            </div>
+        </div>
     </div>
   )
 }
